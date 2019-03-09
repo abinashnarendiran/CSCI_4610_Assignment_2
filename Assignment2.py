33775effd70bfc6e9c1c3549fdd9b9ab00893734
abinashnarendiran <abinash.narendiran@uoit.net>
2019-03-09 15:00:59 -0500

    First Commit

---
+++ Assignment2.py
@@ -0,0 +1,38 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+
+
+
+Population = []
+def addtoPopulation(x,y):
+    n = str(x) + "," +  str(y)
+    Population.append(n)
+
+
+
+addtoPopulation(20,20)
+addtoPopulation(20,40)
+addtoPopulation(20,160)
+addtoPopulation(40, 120)
+addtoPopulation(60,20)
+addtoPopulation(60,80)
+addtoPopulation(60,200)
+addtoPopulation(80,180)
+addtoPopulation(100,40)
+addtoPopulation(100, 120)
+addtoPopulation(100,160)
+addtoPopulation(120,80)
+addtoPopulation(140, 140)
+addtoPopulation(140,180)
+addtoPopulation(160,80)
+addtoPopulation(180, 60)
+addtoPopulation(180,100)
+addtoPopulation(180,200)
+addtoPopulation(200,40)
+addtoPopulation(200,160)
+
+
+
+
